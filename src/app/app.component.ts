c94effe556a003fbe3087bc5a1c8d073721cd049
kurian <kurianck.mail@gmail.com>
2017-02-17 08:23:49 +0530

    adding the models
    template now shows player and the attributes of the player.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Player} from './models/player';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'k';
+  player: Player={
+  score: 29,
+  name:'kurian',
+  id:12
+  };
+  constructor(){
+  }
 }
